7d4f0d6a30f284f19dcc1bfb43c6089fd9c1a982
Samuel5432 <62008635+Samuel5432@users.noreply.github.com>
2020-06-06 10:50:16 +0100

    Create hng01544 submission

---
+++ scripts/hng01544.py
@@ -0,0 +1,6 @@
+fullNme = "Adeniyi Samuel"
+id = "HNG-01544"
+lang = "python"
+emailAddress = "sa7259379@gmail.com"
+
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s" %(fullNme, id, lang, emailAddress), flush=True)
